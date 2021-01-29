15f675956e42da1b6fbe6110f3dc25277db5b317
Oladayo Fagbemi <oofagbemi@outlook.com>
2021-01-29 02:26:02 +0100

    home page finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-     './src/**/*.js',
-  ],
+  purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily:{
-      'lato':['Lato','sans-serif'],
-      'montserrat':['Montserrat','system-ui']
+    fontFamily: {
+      lato: ["Inter var", "sans-serif"],
+      montserrat: ["Inter var", "system-ui"],
     },
-    backgroundColor:theme=>({
-    ...theme('colors'),
-    'primary':'#004968',
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#004968",
+      "primary-dark": "#003850",
+      "primary-light": "#337c9b",
+      secondary: "#ffff00",
+      "secondary-light": "#ffff66",
+      "secondary-dark": "#c6c600",
     }),
-    height:{
-      '100':'30rem'
+    height: {
+      100: "30rem",
     },
     extend: {},
   },
@@ -22,4 +24,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
